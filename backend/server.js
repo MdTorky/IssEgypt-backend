be820960f6dbdcdf58a83fa8c8880f3ed8a2aad5
cargo <mohamed2003torky@gmail.com>
2023-11-21 10:04:31 +0800

    Fixed Something5

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const formRoutes = require('./routes/forms')
-// const cors = require('cors')
 
 // express app
 const app = express()
 
-// middleware
-// app.use(cors({
-//     origin: ['https://localhost:4000'],
-//     methods: ['GET', 'POST', 'DELETE'],
-//     credentials: true
-// }))
+
 app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next()
 })
 
@@ -25,7 +21,9 @@ app.use((req, res, next) => {
 app.use('/api/forms', formRoutes)
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+
+})
     .then(() => {
         console.log('connected to database')
         // listen to port
