e2c879bf30e6a2627bbc867f1f371c4385b7f716
cargo <mohamed2003torky@gmail.com>
2023-11-24 22:04:00 +0800

    trying to fix the image

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://issegypt.vercel.app'); // Set the specific origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
