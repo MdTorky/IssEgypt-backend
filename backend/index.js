6ca0d7027615c1955a81ae352c5cb541955ce11c
cargo <mohamed2003torky@gmail.com>
2023-11-24 22:25:19 +0800

    trying to fix the image

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,18 @@ app.use((req, res, next) => {
 });
 
 
+const allowedOrigins = ['https://issegypt.vercel.app'];
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 
 
 // routes
