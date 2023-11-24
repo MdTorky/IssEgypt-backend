fbc70bd649469d8fd5f260eb5b1d6e6d8dfd87e1
cargo <mohamed2003torky@gmail.com>
2023-11-24 19:26:30 +0800

    trying to fix the image

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -33,7 +33,7 @@ const getMember = async (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '/images')
+        cb(null, '../images')
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
