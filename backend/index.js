77609c9e0ad7d07dbfca8ffe9deacf3e0a5fb804
cargo <mohamed2003torky@gmail.com>
2023-11-27 15:58:30 +0800

    Added Images

--- backend/index.js
+++ backend/index.js
@@ -10,28 +10,18 @@ const path = require('path');
 
 // express app
 const app = express();
-
+app.use(cors());
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Origin', 'https://issegypt.vercel.app');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://issegypt.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
-// app.use((req, res, next) => {
-
-//     next();
-// });
-
-// app.UseCors(x => x
-//     .AllowAnyMethod()
-//     .AllowAnyHeader()
-//     .SetIsOriginAllowed(origin => true) // allow any origin
-//     .AllowCredentials());
 
 
 
