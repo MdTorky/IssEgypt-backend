88d864765229f943d577d9839c5c80dfd5ab0aeb
cargo <mohamed2003torky@gmail.com>
2023-11-27 15:53:30 +0800

    Added Images

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,12 @@ const path = require('path');
 // express app
 const app = express();
 
-app.use(cors());
 
 app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://issegypt.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 
 
 
+app.use(cors());
 
 
 
