cc131a54916cf2cc479dea4ccccedcf0208436b4
cargo <mohamed2003torky@gmail.com>
2024-02-09 14:29:58 +0200

    Added User Auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,46 @@
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+}
+
+
 // Login User
 
 const loginUser = async (req, res) => {
-    res.json({ mssg: "Login User" })
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // Create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({ email, token })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 
 // Register User
 
 const registerUser = async (req, res) => {
-    res.json({ mssg: "Register User" })
+
+    const { email, password, committee } = req.body
+
+    try {
+        const user = await User.register(email, password, committee)
+
+        // Create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({ email, token })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+const Schema = mongoose.Schema
+
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    // userName: {
+    //     type: String,
+    //     // required: true,
+    //     // unique: true
+    // },
+    password: {
+        type: String,
+        required: true,
+    },
+    committee: {
+        type: String,
+        required: true,
+    }
+})
+
+
+// static register method
+userSchema.statics.register = async function (email, password, committee) {
+
+    //validation
+    if (!email || !password || !committee) {
+        throw Error("All fields must be filled")
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid")
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password is not strong enough")
+    }
+
+    const emailExists = await this.findOne({ email })
+    // const userNameExists = await this.findOne({ userName })
+
+    if (emailExists) {
+        throw Error('Email already in use')
+    }
+    // if (userNameExists) {
+    //     throw Error('UserName already in use')
+    // }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash, committee })
+
+    return user
+
+}
+
+
+// static Login Method
+
+userSchema.statics.login = async function (email, password) {
+
+    if (!email || !password) {
+        throw Error("All fields must be filled")
+    }
+
+    const user = await this.findOne({ email })
+
+    if (!user) {
+        throw Error('Incorrect Email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect Password')
+    }
+
+    return user
+
+}
+
+module.exports = mongoose.model('User', userSchema)
